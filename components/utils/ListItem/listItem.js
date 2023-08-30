2ecbe9d11a17431eea6a24583c01c52e420c172a
its-mustafa07 <atta.ul.mustafa.3@gmail.com>
2023-08-30 17:07:56 +0500

    committing to pull

--- components/utils/ListItem/listItem.js
+++ components/utils/ListItem/listItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground } from "react-native";
+import { ImageBackground, Image } from "react-native";
 import {
   AlignItems,
   ImageContainer,
@@ -10,10 +10,8 @@ import {
   ImageText,
   DotSvgImage,
 } from "../ListItem/listItem-styles";
-import { Svg, Ellipse } from "react-native-svg";
 
 export default function ListItem({ cover, heading, paragraph, date, svgIcon }) {
-<<<<<<< HEAD
   return (
     <MainListWrapper>
       <AlignItems>
@@ -32,68 +30,9 @@ export default function ListItem({ cover, heading, paragraph, date, svgIcon }) {
           </TextContainer>
         </ImageText>
         <DotSvgImage>
-          <Svg
-            width="5"
-            height="21"
-            viewBox="0 0 5 21"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <Ellipse
-              cx="2.5"
-              cy="10.165"
-              rx="2.5"
-              ry="2.34577"
-              fill="#06919A"
-            />
-            <Ellipse
-              cx="2.5"
-              cy="17.9842"
-              rx="2.5"
-              ry="2.34577"
-              fill="#06919A"
-            />
-            <Ellipse
-              cx="2.5"
-              cy="2.34577"
-              rx="2.5"
-              ry="2.34577"
-              fill="#06919A"
-            />
-          </Svg>
+          <Image source={svgIcon} />
         </DotSvgImage>
       </AlignItems>
     </MainListWrapper>
   );
-=======
-    return (
-
-        <MainListWrapper>
-            <AlignItems>
-                <ImageText>
-                    <ImageContainer>
-
-                        <ImageBackground source={cover} style={{ flex: 1, }} resizeMode='cover' />
-
-
-                    </ImageContainer>
-                    <TextContainer>
-                        <Text1>{heading}</Text1>
-                        <Text2>{paragraph}</Text2>
-                        <Text2>{date}</Text2>
-                    </TextContainer>
-                </ImageText>
-                <DotSvgImage>
-                    
-                       <Image source={svgIcon}/>
-                
-                </DotSvgImage>
-
-            </AlignItems>
-        </MainListWrapper>
-
-
-
-    )
->>>>>>> a1375b54a02340d8cdc43c50d76bda62e2d82409
 }
--- components/utils/ListView/listView-styles.js
+++ components/utils/ListView/listView-styles.js
@@ -1,26 +1,8 @@
 import { View } from "react-native";
 import { styled } from "styled-components";
 
-<<<<<<< HEAD
 export const ListViewWrapper = styled(View)`
-  flex: 1;
-=======
- export const  ListViewWrapper = styled(View)`
-   
->>>>>>> a1375b54a02340d8cdc43c50d76bda62e2d82409
   display: flex;
-  
-  /* top: 30%; */
   align-items: center;
-<<<<<<< HEAD
-  gap: 30px;
-  /* height: ${({ height }) => height || "auto"}; */
-  /* max-height: calc(95vh - 150px); */
-  /* overflow: auto; */
-  /* padding-bottom: 6.6rem; */
-  /* padding-top: 2rem; */
-`;
-=======
   gap: 15px;
- `
->>>>>>> a1375b54a02340d8cdc43c50d76bda62e2d82409
+`;
